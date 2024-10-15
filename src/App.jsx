d6eb6048fccde77d8fc30ae9cfbcf08f1c7eeb6a
Miftah <miftahaadem@gmail.com>
2024-10-15 20:18:28 +0300

    creating navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <main className="main_container">
         <Outlet />
       </main>
